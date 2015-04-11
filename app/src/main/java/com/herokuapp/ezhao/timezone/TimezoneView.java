0c9a263203723cbe98421a3d093c84e3c99463d2
Emily Zhao <emily@dropbox.com>
2015-04-11 10:53:19 -0700

    ticks that you can drag

--- app/src/main/java/com/herokuapp/ezhao/timezone/TimezoneView.java
+++ app/src/main/java/com/herokuapp/ezhao/timezone/TimezoneView.java
@@ -4,15 +4,20 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 
 public class TimezoneView extends View {
     private Paint testPaint;
+    private Paint tickPaint;
     private float motionStartX;
     private float motionMoveX;
     private float currentOffset;
+    private Path tickMarks;
+    private int tickPadding = 16; // bit of space on either side
+    private int topPadding = 20;
 
     public TimezoneView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -21,14 +26,60 @@ public class TimezoneView extends View {
         testPaint.setStyle(Paint.Style.FILL);
         testPaint.setColor(Color.BLACK);
 
+        tickPaint = new Paint();
+        tickPaint.setStyle(Paint.Style.STROKE);
+        tickPaint.setStrokeWidth(1);
+        tickPaint.setColor(Color.LTGRAY);
+
         currentOffset = 0.0f;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        float WIDTH = getWidth();
+        float HEIGHT = getHeight();
+        float spacing = (3 * WIDTH - 2 * tickPadding) / 24;
+
         float newOffset = motionMoveX - motionStartX;
-        canvas.drawRect(currentOffset+newOffset, 0, 300+currentOffset+newOffset, getHeight(), testPaint);
+        float hiddenAmount = getWidth() - (currentOffset + newOffset);
+        hiddenAmount = Math.max(hiddenAmount, 0.0f);
+        hiddenAmount = Math.min(hiddenAmount, WIDTH * 2.0f);
+
+        // Labels on the hours
+        int displayHour;
+        String displaySuffix;
+        String displayString;
+        float positionX;
+        for (int i = 0; i <= 24; i++) {
+            displayHour = i;
+            displaySuffix = "AM";
+            if (i >= 12) {
+                displayHour = i - 12;
+                displaySuffix = "PM";
+            }
+            if (displayHour == 0) {
+                displayHour = 12;
+            }
+            if (i == 24) {
+                displayHour = 12;
+                displaySuffix = "AM";
+            }
+
+            displaySuffix = String.valueOf(displayHour) + displaySuffix;
+            positionX = i * spacing - hiddenAmount;
+            canvas.drawText(displaySuffix, positionX, topPadding, testPaint);
+        }
+
+        tickMarks = new Path();
+        for (int i = 0; i <= 24; i++) {
+            positionX = i * spacing + tickPadding - hiddenAmount;
+            tickMarks.moveTo(positionX, topPadding);
+            tickMarks.lineTo(positionX, HEIGHT);
+            tickMarks.moveTo(positionX + spacing / 2, topPadding);
+            tickMarks.lineTo(positionX + spacing / 2, HEIGHT / 2);
+        }
+        canvas.drawPath(tickMarks, tickPaint);
     }
 
     @Override
@@ -42,6 +93,8 @@ public class TimezoneView extends View {
                 break;
             case MotionEvent.ACTION_UP:
                 currentOffset += event.getX() - motionStartX;
+                currentOffset = Math.min(currentOffset, getWidth());
+                currentOffset = Math.max(currentOffset, -getWidth());
                 motionMoveX = 0.0f;
                 motionStartX = 0.0f;
                 break;
